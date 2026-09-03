4dd1e38cd25054b4eb723114c65958be8c2462d9
Matthew45678 <mzel2@onet.pl>
2026-09-03 22:46:52 +0200

    Add focus functionality

--- Assets/Interactions.cs
+++ Assets/Interactions.cs
@@ -4,69 +4,74 @@ using UnityEngine;
 
 public class Interactions : MonoBehaviour
 {
-    private float range = 1f;
     public Color interactionColor = Color.blue;
+    [SerializeField] private KeyCode _interactKey = KeyCode.E;
+    
+    private readonly List<GameObject> _interactions = new List<GameObject>();
 
-    private Dictionary<int, Color> colors;
-    private List<GameObject> interactions;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        colors = new Dictionary<int, Color>();
-        interactions = new List<GameObject>();
-    }
+    private DeskWorkstation _occupiedDesk;
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && !interactions.Contains(null))
+        if (_occupiedDesk && !_occupiedDesk.IsSeated && !_occupiedDesk.IsBusy)
+            _occupiedDesk = null;
+        if (!Input.GetKeyDown(_interactKey)) return;
+
+        //0) Wstać od biurka
+        if (_occupiedDesk)
         {
-            GameObject obj = interactions[0];
-            if (obj.tag == "Opened")
-            {
-                obj.GetComponent<Animator>().SetBool("Open", true);
-                obj.tag = "Closed";
-            }
-            else if (obj.tag == "Closed")
-            {
-                obj.GetComponent<Animator>().SetBool("Open", false);
-                obj.tag = "Opened";
-            }
-
+            _occupiedDesk.Interact();
+            return;
         }
+
+        GameObject obj = GetClosest();
+        if(!obj) return;
+
+        //1) biurko i timer
+        var desk = obj.GetComponentInParent<DeskWorkstation>();
+        if (desk)
+        {
+            desk.Interact();
+            _occupiedDesk = desk;
+            return;
+        }
+        
+        //2) drzwi/szafki
+        var animator = obj.GetComponent<Animator>();
+        if (animator)
+        {
+            bool isOpen = animator.GetBool("Open");
+            animator.SetBool("Open", !isOpen);
+            return;
+        }
+
+        //3)
+        obj.SendMessage("OnInteract", SendMessageOptions.DontRequireReceiver);
     }
 
-
-    void OnTriggerEnter(Collider other)
+    private GameObject GetClosest()
     {
-        Debug.Log(other.gameObject.name + "Entered");
-        if (colors == null)
-            colors = new Dictionary<int, Color>();
-        if (interactions == null)
-            interactions = new List<GameObject>();
-        if (other.gameObject.layer != 6)
-            return;
-        // Debug.Log(other.gameObject.name);
-        // Material material = other.gameObject.GetComponent<MeshRenderer>().materials[0];
-        // Debug.Log(material.GetColor("_SecondShadeMaskColor"));    
-        // Color color = other.gameObject.GetComponent<MeshRenderer>().materials[0].GetColor("_2nd_ShadeColor");
-        // Debug.Log(color);
-        // other.gameObject.GetComponent<MeshRenderer>().materials[0].SetColor("_2nd_ShadeColor",interactionColor);
-        // Debug.Log(color);
-        // Debug.Log(other.gameObject.GetInstanceID());
-        // Debug.Log(colors);
-        // colors.Add(other.gameObject.GetInstanceID(), color);
-        // Debug.Log("Interaction possible");
-        interactions.Add(other.gameObject);
-
+        _interactions.RemoveAll(o => !o);
+        GameObject best = null;
+        float bestDistance = float.MaxValue;
+        foreach (var o in _interactions)
+        {
+            float d = (o.transform.position - transform.position).sqrMagnitude;
+            if(d < bestDistance) {bestDistance = d; best = o; }
+        }
+        return best;
     }
 
-    void OnTriggerExit(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer != 6)
-            return;
-        // other.gameObject.GetComponent<MeshRenderer>().materials[0].SetColor("_2nd_ShadeColor",colors[other.gameObject.GetInstanceID()]);
-        // colors.Remove(other.gameObject.GetInstanceID());
-        interactions.Remove(other.gameObject);
+        if(other.gameObject.layer != 6) return;
+        if(!_interactions.Contains(other.gameObject))
+            _interactions.Add(other.gameObject);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.layer != 6) return;
+        _interactions.Remove(other.gameObject);
     }
 }
--- Assets/timer_service.cs
+++ Assets/timer_service.cs
@@ -2,144 +2,231 @@ using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using Unity.Cinemachine;
+
+public enum PomodoroPhase
+{
+    Idle,
+    Work,
+    Break
+}
 
 public class timer_service : MonoBehaviour
 {
-    private List<TimerHistory> timerHistory = new List<TimerHistory>();
-    private float startTime;
-    private bool timerStarted = false;
-    private bool timerPaused = false;
 
-    public float Minutes = 10;
-    public float Seconds = 0;
-    public TextMeshProUGUI Text;
+    [Header("Domyślne wartości pomodoro")] [SerializeField]
+    private float _DefaultWorkMinutes = 25f;
 
-    private float _time;
-    private float _remainingTime;
-    private float _timerTemp;
-    private bool statechanged = false;
+    [SerializeField] private float _DefaultBreakMinutes = 5f;
+    [SerializeField] private bool _loopPhases = true;
 
-    private bool displayRemainingTime = false;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Wyświetlanie (Ekran na biurku)")] [SerializeField]
+    private TMP_Text _timeText;
+
+    [SerializeField] private TMP_Text _stateText;
+    [SerializeField] private string _workLabel = "PRACA";
+    [SerializeField] private string _breakLabel = "PRZERWA";
+    [SerializeField] private string _idleLabel = "-";
+
+
+    [Header("Tło")] [SerializeField] private Renderer _backgroundRenderer;
+    [SerializeField] private string _colorProperty = "_BaseColor";
+    [SerializeField] private Graphic _backgroundGraphic;
+    [SerializeField] private Color _workColor;
+    [SerializeField] private Color _breakColor;
+    [SerializeField] private Color _idleColor;
+    [SerializeField] private bool _tintText = false;
+    
+    [Header("Animacja gracza (poza siadaniem")] [SerializeField]
+    private Animator _playerAnimator;
+
+    [SerializeField] private string _breakBoolParam = "Focus";
+
+    //Zdarzenia zewnętrzne
+    public event Action<PomodoroPhase> OnPhaseChange;
+    public event Action<float> OnTick;
+    public event Action OnFinished;
+
+    //Stan
+    private PomodoroPhase _phase = PomodoroPhase.Idle;
+    private float _workDuration, _breakDuration;
+    private float _time, _timerTemp, _remainingTime;
+    private bool _running;
+
+    public PomodoroPhase phase => _phase;
+    public float RemainingTime => _remainingTime;
+    public float PhaseDuration => _phase == PomodoroPhase.Work ? _workDuration : _timerTemp;
+    public bool IsRunning => _running;
+    public string TimeString => Format(_remainingTime);
+
+    public string StateString => _phase == PomodoroPhase.Work ? _workLabel
+        : _phase == PomodoroPhase.Break ? _breakLabel
+        : _idleLabel;
+
+    private void Awake()
     {
-        _time = Minutes * 60 + Seconds;
+        _workDuration = _DefaultWorkMinutes * 60f;
+        _breakDuration = _DefaultBreakMinutes * 60f;
+        ApplyVisuals();
+        RefreshDisplay();
     }
 
-    // Update is called once per frame
-    void Update()
+    //API
+    public void SetLoop(bool loop) => _loopPhases = loop;
+    
+    public void Configure(float workMinutes, float breakMinutes)
     {
-        if (Input.GetKeyDown(KeyCode.M))
-        {
-            displayRemainingTime = !displayRemainingTime;
-            statechanged = true;
-        }
-        else if (Input.GetKeyDown(KeyCode.F) && !timerStarted)
-        {
-            //Start  timer
-            startTime = Time.time;
-            _time = 0;
-            _remainingTime = Minutes * 60 + Seconds;
-            timerStarted = true;
-            statechanged = true;
-        }
-
-        else if (Input.GetKeyDown(KeyCode.F) && timerStarted)
-        {
-            if (!timerPaused)
-            {
-                //Pause timer
-                timerPaused = true;
-                statechanged = true;
-            }
-            else
-            {
-                //Resume timer
-                timerPaused = false;
-                statechanged = true;
-            }
-        }
-        else if (Input.GetKeyDown(KeyCode.X) && timerStarted)
-        {
-            //Stop timer
-            timerStarted = false;
-            TimerHistory history = new TimerHistory();
-            history.startTime = startTime;
-            history.endTime = Time.time;
-            history.completed = false;
-            history.totalTime = Minutes * 60 + Seconds;
-            history.elapsedTime = _time;
-            timerHistory.Add(history);
-            statechanged = true;
-            
-        }
-
-        if ((timerStarted && !timerPaused ))
-        {
-            _timerTemp = _time;
-            _time += Time.deltaTime;
-            _remainingTime -= Time.deltaTime;
-            if (_remainingTime < 0 || _time > Minutes * 60 + Seconds)
-            {
-                timerStarted = false;
-                TimerHistory history = new TimerHistory();
-                history.startTime = startTime;
-                history.endTime = Time.time;
-                history.completed = true;
-                history.totalTime = Minutes * 60 + Seconds;
-                history.elapsedTime = _time;
-                timerHistory.Add(history);
-            }
-            //check if timertemp and time variables have the same seconds or not
-            
-        }
-        if ((int)_time != (int)_timerTemp || _time == 0 || statechanged)
-        {
-            TimeSpan t;
-            if (displayRemainingTime)
-            {
-                t = TimeSpan.FromSeconds(_remainingTime);
-
-            }
-            else
-            {
-                t = TimeSpan.FromSeconds(_time);
-            }
-            Text.text = t.Hours > 0
-                ? string.Format("{0:D2}:{1:D2}:{2:D2}", t.Hours, t.Minutes, t.Seconds)
-                : string.Format("{0:D2}:{1:D2}", t.Minutes, t.Seconds);
-
-            if (statechanged && timerPaused)
-            {
-                Text.color = Color.yellow;
-            }
-
-            if (statechanged && !timerPaused)
-            {
-                Text.color = Color.white;
-            }
-
-            if (!timerStarted)
-            {
-                Text.gameObject.SetActive(false);
-            }
-
-            if (timerStarted && !Text.gameObject.activeInHierarchy)
-            {
-                Text.gameObject.SetActive(true);
-            }
-        }
- 
+        _workDuration = Mathf.Max(1f, workMinutes * 60f);
+        _breakDuration = Mathf.Max(1f, breakMinutes * 60f);
     }
-}
 
-struct TimerHistory
-{
+    public void ConfigureSeconds(float workSeconds, float breakSeconds)
+    {
+        _workDuration = Mathf.Max(1f, workSeconds);
+        _breakDuration = Mathf.Max(1f, breakSeconds);
+    }
 
-       public float startTime;
-       public float endTime;
-       public bool completed;
-       public float totalTime;
-       public float elapsedTime;
+    public void StartTimer() => EnterPhase(PomodoroPhase.Work);
+    public void StartWork() => EnterPhase(PomodoroPhase.Work);
+    public void StartBreak() => EnterPhase(PomodoroPhase.Break);
 
+    public void StartTimer(float workMinutes, float breakMinutes)
+    {
+        Configure(workMinutes, breakMinutes);
+        EnterPhase(PomodoroPhase.Work);
+    }
+
+    public void Pause() => _running = false;
+
+    public void Resume()
+    {
+        if (_phase != PomodoroPhase.Idle)
+        {
+            _running = true;
+        }
+    }
+
+    public void SkipPhase()
+    {
+        if(_phase == PomodoroPhase.Idle)
+            HandlePhaseEnd();
+    }
+
+    public void Stop()
+    {
+        _running = false;
+        _phase = PomodoroPhase.Idle;
+        _remainingTime = 0f;
+        _time = 0f;
+        _timerTemp = 0f;
+        SetAnimatorBreak(false);
+        ApplyVisuals();
+        RefreshDisplay();
+        OnPhaseChange?.Invoke(_phase);
+    }
+
+    //Pętla
+    private void Update()
+    {
+        if (!_running) return;
+
+        _timerTemp = _time;
+        _time += Time.deltaTime;
+        _remainingTime -= Time.deltaTime;
+
+        if (_remainingTime <= 0f)
+        {
+            _remainingTime = 0;
+            RefreshDisplay();
+            OnTick?.Invoke(0f);
+            HandlePhaseEnd();
+            return;
+        }
+
+        if ((int)_time != (int)_timerTemp)
+        {
+            RefreshDisplay();
+            OnTick?.Invoke(_remainingTime);
+        }
+    }
+
+    private void HandlePhaseEnd()
+    {
+        if (_phase == PomodoroPhase.Work)
+        {
+            EnterPhase(PomodoroPhase.Break);
+        }
+        else if (_phase == PomodoroPhase.Break)
+        {
+            if(_loopPhases)
+                EnterPhase(PomodoroPhase.Work);
+            else
+            {
+                Stop();
+                OnFinished?.Invoke();
+            }
+        }
+    }
+
+    private void EnterPhase(PomodoroPhase phase)
+    {
+        _phase = phase;
+        _remainingTime = _phase == PomodoroPhase.Work ? _workDuration : _breakDuration;
+        _time = 0f;
+        _timerTemp = 0f;
+        _running = true;
+        
+        SetAnimatorBreak(phase == PomodoroPhase.Break);
+        ApplyVisuals();
+        RefreshDisplay();
+        OnPhaseChange?.Invoke(_phase);
+
+    }
+
+    //prezentacja
+    private void RefreshDisplay()
+    {
+        if (_timeText) _timeText.text = TimeString;
+        if(_stateText) _stateText.text = StateString;
+    }
+
+    private void ApplyVisuals()
+    {
+        Color color = _phase == PomodoroPhase.Work ? _workColor
+            : _phase == PomodoroPhase.Break ? _breakColor
+            : _idleColor;
+
+        if (_backgroundRenderer)
+        {
+            var mat = _backgroundRenderer.material;
+            if (mat.HasProperty(_colorProperty)) mat.SetColor(_colorProperty, color);
+            else mat.color = color;
+        }
+
+        if (_backgroundGraphic)
+        {
+            _backgroundGraphic.color = color;
+        }
+
+        if (_tintText)
+        {
+            if(_timeText) _timeText.color = color;
+            if(_stateText) _stateText.color = color;
+        }
+        
+    }
+
+    private static string Format(float seconds)
+    {
+        TimeSpan t = TimeSpan.FromSeconds(Mathf.Max(0f, seconds));
+        return t.Hours>0? $"{t.Hours:D2}:{t.Minutes:D2}:{t.Seconds:D2}" : $"{t.Minutes:D2}:{t.Seconds:D2}";
+        
+    }
+
+    private void SetAnimatorBreak(bool onBreak)
+    {
+        if (_playerAnimator || string.IsNullOrEmpty(_breakBoolParam)) return;
+        _playerAnimator.SetBool(_breakBoolParam, onBreak);
+    }
 }
